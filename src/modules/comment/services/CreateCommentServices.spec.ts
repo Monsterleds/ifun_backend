8f370f7a210bdf3770f9134b6db2f46f706c594c
unknown <kha.led002@hotmail.com>
2020-07-11 16:21:03 -0300

    feature: comments tests

---
+++ src/modules/comment/services/CreateCommentServices.spec.ts
@@ -0,0 +1,60 @@
+import "reflect-metadata";
+
+import FakeCommentRepository from '../repositories/fakes/FakeCommentRepositories';
+import CreateCommentServices from './CreateCommentServices';
+
+import FakePostsRepositories from '@modules/post/repositories/fakes/FakePostsRepositories';
+import CreatePostServices from '@modules/post/services/CreatePostServices';
+
+import FakeUsersRepositories from '@modules/user/repositories/fakes/FakeUsersRepositories';
+import CreateUserServices from '@modules/user/services/CreateUserServices';
+
+import AppError from '@shared/errors/AppError';
+
+describe('CreateComment', () => {
+  it('should be able to create a comment on post', async () => {
+    const fakeUsersRepositories = new FakeUsersRepositories();
+    const createUserServices = new CreateUserServices(fakeUsersRepositories);
+    
+    const fakePostsRepositories = new FakePostsRepositories();
+    const createPostServices = new CreatePostServices(fakeUsersRepositories, fakePostsRepositories);
+
+    const fakeCommentRepository = new FakeCommentRepository();
+    const createCommentServices = new CreateCommentServices(fakePostsRepositories, fakeCommentRepository);
+
+    const user = {
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456'
+    }
+
+    const newUser = await createUserServices.execute(user);
+
+    if(!newUser?.id) {
+      throw new AppError('User need a id', 400)
+    }
+
+    const post = {
+      id_user: newUser.id,
+      title: 'funny hehe',
+      subtitle: 'awd',
+      description: 'HAHAHA COOL',
+      avatar_id: ''
+    };
+
+    const { id } = await createPostServices.execute(post)
+
+    const comment = await createCommentServices.execute({ id_post: id, description: 'WOWWW COOOOL' });
+
+    expect(comment).toHaveProperty('id');
+  })
+
+  it('should not be able to create a comment on post with wrong post id', async () => {
+    const fakePostsRepositories = new FakePostsRepositories();
+
+    const fakeCommentRepository = new FakeCommentRepository();
+    const createCommentServices = new CreateCommentServices(fakePostsRepositories, fakeCommentRepository);
+ 
+    await expect(createCommentServices.execute({ id_post: 'wrong id', description: 'WOWWW COOOOL' })).rejects.toBeInstanceOf(AppError);
+  })
+})
--- src/modules/comment/services/CreateCommentServices.ts
+++ src/modules/comment/services/CreateCommentServices.ts
@@ -1,6 +1,9 @@
 import { inject, injectable } from 'tsyringe';
 
 import ICommentsRepositories from '../repositories/ICommentsRepositories';
+import IPostsRepositories from '@modules/post/repositories/IPostsRepositories';
+
+import AppError from '@shared/errors/AppError';
 
 interface Request {
   description: string;
@@ -10,10 +13,19 @@ interface Request {
 @injectable()
 class CreateCommentServices {
   constructor(
+    @inject('PostsRepositories')
+    private postsRepositories: IPostsRepositories,
     @inject('CommentsRepositories')
-    private commentsRepositories: ICommentsRepositories) {}
+    private commentsRepositories: ICommentsRepositories
+    ) {}
 
   public async execute({ description, id_post }: Request) {
+    const post = await this.postsRepositories.findById(id_post);
+
+    if(!post) {
+      throw new AppError('Post does not exists', 400);
+    }
+
     const comment = await this.commentsRepositories.create({ description, id_post });
 
     return comment;
